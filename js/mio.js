a0e34cf867fded80ecf20c3a08a95325c3ed5f4e
Mathyptr <mathyptr@gmail.com>
2021-03-06 22:06:31 +0100

    Inserimento file

---
+++ js/mio.js
@@ -0,0 +1,71 @@
+	function jsGet(exp){
+		var xhttp = new XMLHttpRequest();
+			xhttp.onreadystatechange = function() {
+				if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("operazioni").value = this.responseText;
+				}
+			};
+			xhttp.open("GET"," http://api.mathjs.org/v4/?expr="+exp, true);
+			xhttp.send();
+	}
+
+	function jqueryGet(exp){
+		$.get( "http://api.mathjs.org/v4/?expr="+exp, function( data ) {
+			document.getElementById("operazioni").value = data; 
+			});
+	}
+
+	function jsPost(exp){
+			  var xhttp = new XMLHttpRequest();
+		  xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+			obj = JSON.parse(this.responseText);
+			  document.getElementById("operazioni").value = obj.result;
+			}
+		  };
+		  xhttp.open("POST", "http://api.mathjs.org/v4/", true);
+		xhttp.setRequestHeader("Content-Type","application/json");
+		  xhttp.send(  JSON.stringify({"expr":exp}));
+	}
+
+	function jqueryPost(exp){
+	jQuery.ajax ({
+			url:  "http://api.mathjs.org/v4/",
+			type: "POST",
+			data: JSON.stringify({"expr":exp}),
+			dataType: "json",
+			contentType: "application/json",
+			success: function(data,status){
+			 document.getElementById("operazioni").value = data.result;
+			}
+		});
+	}
+
+	function Check(){
+		var list= document.getElementById("list");
+		var opt = list.options[list.selectedIndex];
+		return opt.value;
+		}
+
+	function getButton(dato){ 
+		   document.getElementById("operazioni").value+=dato;
+		}
+
+		function Calculate() { 
+		var opt=Check();
+		var expGet=encodeURIComponent(document.getElementById("operazioni").value);
+		var expPost=document.getElementById("operazioni").value;
+		if(opt=="jsGet")
+		jsGet(expGet);
+		else if(opt=="jqueryGet")
+		jqueryGet(expGet);
+		else if (opt=="jsPost")
+		jsPost(expPost);
+		else if (opt=="jqueryPost")
+		jqueryPost(expPost);
+		}
+
+
+		function cancella() { 
+		 document.getElementById("operazioni").value=""; 
+		}
